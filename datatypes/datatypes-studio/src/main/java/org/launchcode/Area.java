e8b585bdce73ab1cdeb14899719b6da14346e501
milhousecvh <135559562+milhousecvh@users.noreply.github.com>
2023-08-16 19:29:39 -0500

    class 2 studio

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,13 +6,12 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("What is the radius?");
-        int radius = input.nextInt();
-        double pi = 3.14;
-        double result = area(pi, radius);
-        System.out.println(result);
+        double radius = input.nextDouble();
+        double result = Circle.getArea(radius);
+        System.out.printf(String.format("The area of a circle of radius %s is: %s%n", radius, result));
     }
-
-    public static double area(double num1, double num2) {
-        return num1 * num2 * num2;
-    }
+//
+//    public static double area(double num1, double num2) {
+//        return num1 * num2 * num2;
+//    }
 }
