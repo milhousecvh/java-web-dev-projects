0d79a141fac4c617bb43a0243f22e16daedaf1ff
milhousecvh <135559562+milhousecvh@users.noreply.github.com>
2023-08-16 19:34:48 -0500

    class 2 studio - bonuses added

--- control-flow-and-collections/studio/src/studio/CountingCharacters.java
+++ control-flow-and-collections/studio/src/studio/CountingCharacters.java
@@ -1,12 +1,16 @@
 package studio;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CountingCharacters {
     public static void main(String[] args) {
-
-        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);
+//        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         HashMap<Character, Integer> letters = new HashMap<>();
+
+        System.out.println("Tell me a quote:");
+        String quote = input.nextLine();
 
         for (char c : quote.toCharArray()) {
             letters.put(c, letters.getOrDefault(c, 0) + 1);
